a187dca76ec00d775d32e66fe83aa6fd39d92972
beepscore <support@beepscore.com>
2015-02-28 00:04:27 -0800

    Add NameViewController. In ViewController nameButton action add segue to NameViewController.

---
+++ PrepareForSegueSetsBlock/NameViewController.h
@@ -0,0 +1,13 @@
+//
+//  NameViewController.h
+//  PrepareForSegueSetsBlock
+//
+//  Created by Steve Baker on 2/27/15.
+//  Copyright (c) 2015 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NameViewController : UIViewController
+
+@end
